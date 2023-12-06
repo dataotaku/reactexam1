0afc13ad66af87bfb1dc043b77bde8afc8f18aa7
unho.chang <capcock@snu.ac.kr>
2023-12-06 18:43:12 +0900

    학습완료 커밋

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 //import logo from './logo.svg';
 import './App.css';
 
+import MyHeader from "./MyHeader";
+import MyFooter from './MyFooter';
+
 function App() {
 
   let name = "장운호"
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>안녕 리액트 {name} </h2>
-      </header>
+      <MyHeader/>
+      <h2>안녕 리액트 {name} </h2>
+      <b id='bold_text'>React.js</b>
+      <MyFooter />
     </div>
   );
 }
